568bf99a41095154e42fc3eab6557dfc82140e01
poojagautam89 <poojaprachi448@gmail.com>
2025-11-18 17:03:28 +0530

    spring boot project setup and neonDB setup

---
+++ src/main/java/com/urssystems/telecom_backend/TelecomBackendApplication.java
@@ -0,0 +1,13 @@
+package com.urssystems.telecom_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelecomBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelecomBackendApplication.class, args);
+	}
+
+}
